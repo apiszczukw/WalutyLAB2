911f698ce807432ee66b996e130ee5f4d39be1c6
apiszczukw <apiszczukw@gmail.com>
2025-03-29 09:31:13 +0100

    Dodaj pliki projektów.

---
+++ WalutyLAB2/Klasy/NBPAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalutyLAB2.Klasy
+{
+    internal class NBPAPI
+    {
+        public static string daneNBP = "https://static.nbp.pl/dane/kursy/xml/LastA.xml";
+    }
+}
---
+++ WalutyLAB2/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
+using WalutyLAB2.Klasy;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace WalutyLAB2
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        List<PozycjaTabA> kursyWalut = new List<PozycjaTabA>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var klient = new HttpClient();
+            var dane = await klient.GetStringAsync(new Uri(NBPAPI.daneNBP));
+
+            var daneXml = XDocument.Parse(dane);
+
+            foreach (var pozycja in daneXml.Descendants("pozycja"))
+            {
+                var pozycjaTabA = new PozycjaTabA()
+                {
+                    NazwaWaluty = pozycja.Element("nazwa_waluty").Value,
+                    Przelicznik = pozycja.Element("przelicznik").Value,
+                    KodWaluty = pozycja.Element("kod_waluty").Value,
+                    KursSredni = pozycja.Element("kurs_sredni").Value
+                };
+
+                kursyWalut.Add(pozycjaTabA);
+            }
+
+
+            // ustawiamy listę na naszych listboxach
+            WalutaWejscieLbx.ItemsSource = kursyWalut;
+            WalutaWyjscieLbx.ItemsSource = kursyWalut;
+        }
+    }
+}
